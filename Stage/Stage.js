af4b1fad4b06015ef136167e38e9cd0afa9f86af
Code Sandbox <hello@codesandbox.io>
2022-06-13 07:57:33 +0000

    Deployment from Codesandbox.

---
+++ Stage/Stage.js
@@ -0,0 +1,33 @@
+/* eslint-disable require-yield, eqeqeq */
+
+import {
+  Stage as StageBase,
+  Trigger,
+  Costume,
+  Color,
+  Sound
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+export default class Stage extends StageBase {
+  constructor(...args) {
+    super(...args);
+
+    this.costumes = [
+      new Costume("backdrop1", "./Stage/costumes/backdrop1.svg", {
+        x: 240,
+        y: 180
+      }),
+      new Costume("Xy-grid", "./Stage/costumes/Xy-grid.png", {
+        x: 480,
+        y: 360
+      }),
+      new Costume("Stars", "./Stage/costumes/Stars.png", { x: 480, y: 360 })
+    ];
+
+    this.sounds = [new Sound("pop", "./Stage/sounds/pop.wav")];
+
+    this.triggers = [];
+
+    this.vars.myVariable = 0;
+  }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+import { Project } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+import Stage from "./Stage/Stage.js";
+import Sprite1 from "./Sprite1/Sprite1.js";
+
+const stage = new Stage({ costumeNumber: 3 });
+
+const sprites = {
+  Sprite1: new Sprite1({
+    x: 100,
+    y: 100,
+    direction: 83.8586905884871,
+    costumeNumber: 1,
+    size: 100,
+    visible: true
+  })
+};
+
+const project = new Project(stage, sprites);
+export default project;
